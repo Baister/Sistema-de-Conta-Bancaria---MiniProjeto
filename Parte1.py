d093e14f77d3f320249d0a7024d96cd8ace82897
Baister <gustavobaister@gmail.com>
2026-02-10 23:10:08 -0300

    Criando Métodos

---
+++ Parte1.py
@@ -0,0 +1,31 @@
+#Criando mini projeto - com Classe kkkk
+import os
+def limpa_tela():
+    os.system('cls')
+
+class Conta():
+    def __init__(self,banco, agencia, conta):
+        self.banco = banco
+        self.agencia = agencia
+        self.conta = conta
+        self.saldo = 0 #Colocando valor Default
+
+    def depositar(self,deposito):
+        if deposito <= 0:
+            raise ValueError("Favor fazer um depósito maior que zero.")
+        
+        self.saldo += deposito
+
+    def sacar(self,sacado):
+        self.saldo -= sacado
+
+    def __str__(self):
+        return (f'Agência: {self.agencia}\n'
+                f'Conta: {self.conta}\n'
+                f'Saldo: R${self.saldo:.2f}')
+
+limpa_tela()
+conta_PagBank = Conta('PagBank', '3219','42931')
+conta_PagBank.depositar(500.21)
+conta_PagBank.sacar(300)
+print(conta_PagBank)
